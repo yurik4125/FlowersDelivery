720b1394a51b1c2d2891a5a1496f9b9ec98ab423
yurik4125 <yurik4125@gmail.com>
2019-04-13 08:14:03 -0400

    Login

---
+++ Flowers_web/Controllers/AuthorizationController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Flowers_web.Models;
+
+namespace Flowers_web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private a206yuriyEntities db = new a206yuriyEntities();
+
+        // GET: Authorization
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.Group);
+            return View(users.ToList());
+        }
+
+        // GET: Authorization/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Authorization/Create
+        public ActionResult Create()
+        {
+            ViewBag.User_of_Group = new SelectList(db.Groups, "ID", "Name_Group");
+            return View();
+        }
+
+        // POST: Authorization/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "User_ID,User_Name,User_Pass,User_Status,User_of_Group")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.User_of_Group = new SelectList(db.Groups, "ID", "Name_Group", user.User_of_Group);
+            return View(user);
+        }
+
+        // GET: Authorization/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.User_of_Group = new SelectList(db.Groups, "ID", "Name_Group", user.User_of_Group);
+            return View(user);
+        }
+
+        // POST: Authorization/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "User_ID,User_Name,User_Pass,User_Status,User_of_Group")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.User_of_Group = new SelectList(db.Groups, "ID", "Name_Group", user.User_of_Group);
+            return View(user);
+        }
+
+        // GET: Authorization/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Authorization/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string User, string Pass)
+        {
+            if (string.IsNullOrEmpty(User) || string.IsNullOrEmpty(Pass))
+            {
+                return View();
+            }
+            var user=db.Users.FirstOrDefault(d=>d.User_Name.Equals(User) && d.User_Pass.Equals(Pass));
+            if (user == null)
+            {
+                return View();
+            }
+
+            //if (user.Group.Name_Group.Equals(Role.Manager.ToString()))
+            {
+                string userName = user.User_ID.ToString();
+                //CreateCookie(userName);
+                // FormsAuthentication.SetAuthCookie(userName, true);
+                // do OTP validation
+                 Response.SetCookie(CreateCookie(userName));
+                // Response.Flush();
+                return this.RedirectToAction("Index", "Manager");
+
+            }
+            return View();
+        }
+        private HttpCookie CreateCookie(string value)
+        {
+            HttpCookie CookiesFlowers = new HttpCookie("FlowersCookie")
+            {
+                Value = value,
+                Expires = DateTime.Now.AddSeconds(30)
+            };
+            return CookiesFlowers;
+        }
+    }
+
+    enum Role
+    {
+        Manager=1,
+        CallCenter=2,
+        User=3,
+        Admin =4
+    }
+}
---
+++ Flowers_web/Controllers/ManagerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flowers_web.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            var cookie = Request.Cookies["FlowersCookie"];
+            if (cookie != null)
+            {
+                ViewBag.id = cookie.Value;
+                return View();
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
