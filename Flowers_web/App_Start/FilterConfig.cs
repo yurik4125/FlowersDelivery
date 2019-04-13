bb847d837e0bba2eb2e4bd56a6f013cf89ad764d
yurik4125 <yurik4125@gmail.com>
2019-04-13 06:40:25 -0400

    Add project files.

---
+++ Flowers_web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Flowers_web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
