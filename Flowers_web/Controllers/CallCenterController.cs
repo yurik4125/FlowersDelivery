3f6b7b9f1576d2d82bae02ea30b5b7a1d1f77288
Yuriy Zhukov <yurik4125@gmail.com>
2019-05-28 12:13:56 -0400

    Revert "add find cust"
    This reverts commit ef055fbb091b707d60acf0f7cc5ad5711616a6f5.

--- Flowers_web/Controllers/CallCenterController.cs
+++ Flowers_web/Controllers/CallCenterController.cs
@@ -27,11 +27,6 @@ namespace Flowers_web.Controllers
                 return View("Error");
             }
         }
-       // [HttpPost]
-        public ActionResult SearchUser(string search)
-        {
-            return Json("chamara", JsonRequestBehavior.AllowGet);
-        }
         [HttpPost]
         public ActionResult Calculate(CallCenter model)
         {
