8883da4a6eb8ef564486d4f122dc518c66bbf2ae
Yuriy Zhukov <yurik4125@gmail.com>
2019-06-13 14:21:38 -0400

    Delivery page created

---
+++ Flowers_web/Controllers/DeliveryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Flowers_web.Models;
+
+
+namespace Flowers_web.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        Delivery model;
+        flowersEntities1 db = new flowersEntities1();
+        public DeliveryController()
+        {
+            model =  Delivery.Instance();
+        }
+
+        // GET: Delivery
+        public ActionResult Index()
+        {
+            var order = from o in db.Order_Del select o;
+            //if (!String.IsNullOrEmpty(searching))
+            //{
+            //    order = order.Where(s => s.Order_Status.Contains(searching));
+            //}
+            return View(order.ToList());
+        }
+        [HttpPost]
+        public ActionResult GetOrders(string searching)
+        {          
+            
+            if (!String.IsNullOrEmpty(searching))
+            {
+                return View("Index",model.GetOrderList(searching));               
+            }
+            return View();
+        }
+    }
+}
---
+++ Flowers_web/Models/Delivery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Flowers_web.Models
+{
+    public class Delivery
+    {
+        flowersEntities1 db;
+        List<Order_Del> OrderList { get; set; }
+        static Delivery Delivery2 { get; set; }
+        protected Delivery()
+        {
+            db = new flowersEntities1();
+            OrderList = new List<Order_Del>();
+        }
+
+        public static Delivery Instance()
+        {
+            if (Delivery2 == null)
+            {
+                Delivery2=new Delivery();
+                return Delivery2;
+            }
+            else
+            {
+                return Delivery2;
+            }
+        }
+
+        public List<Order_Del> GetOrderList(string searching)
+        {
+            DateTime date;
+            DateTime.TryParse(searching, out date);
+            OrderList = db.Order_Del.Where(s => s.Date_Delived.Value.Year == date.Year &&
+            s.Date_Delived.Value.Month == date.Month &&
+            s.Date_Delived.Value.Day == date.Day).ToList();
+            return OrderList;
+        }
+    }
+}
